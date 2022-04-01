0b3c01c39ed7209edd9167a383d6a7fb160cb612
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-04-01 14:27:47 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+
 import TodoApp2 from './components/TodoApp2';
+
 
 
 function App() {
   return (
     <div className="App">
       <p> <b> The List </b> </p>
+      
       <TodoApp2 />
 
     </div>
